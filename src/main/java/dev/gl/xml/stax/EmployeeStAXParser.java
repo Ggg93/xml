ea2d936b63a88988636333270918821eeb9624b7
gl <g.lipovtsev@gmail.com>
2025-04-21 15:49:49 +0300

    Tests for StAX parser prepared

--- src/main/java/dev/gl/xml/stax/EmployeeStAXParser.java
+++ src/main/java/dev/gl/xml/stax/EmployeeStAXParser.java
@@ -13,11 +13,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.Attribute;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
@@ -29,126 +30,142 @@ import javax.xml.stream.events.XMLEvent;
 public class EmployeeStAXParser {
 
     private static final Logger LOGGER = Logging.getLocalLogger(EmployeeStAXParser.class);
+    private final File file;
+
+    public EmployeeStAXParser(File file) {
+
+        if (file == null || !file.exists()) {
+            throw new IllegalArgumentException("Input file is not exist!");
+        }
+
+        this.file = file;
+    }
 
     public static void main(String[] args) {
-        
+
         File file = ExamplesStorage.getFile(ExamplesStorage.EMPLOYEE_XML_PATH);
+
+        EmployeeStAXParser parser = new EmployeeStAXParser(file);
+        Employee employee = parser.parseEmployee();
+        LOGGER.info(employee.toString());
+    }
+
+    public Employee parseEmployee() {
+
+        Employee employee = null;
 
         try {
             InputStream is = new FileInputStream(file);
             XMLInputFactory factory = XMLInputFactory.newFactory();
             XMLEventReader reader = factory.createXMLEventReader(is);
 
-            Employee employee = parseEmployee(reader);
-            LOGGER.info(employee.toString());
+            Group group = null;
+            Contact contact = null;
 
-        } catch (Exception e) {
-            LOGGER.severe("Exception occured: "
-                    + e.getLocalizedMessage() + ": " + e.getLocalizedMessage());
-            e.printStackTrace(System.err);
-        }
+            while (reader.hasNext()) {
+                XMLEvent event = reader.nextEvent();
+                if (event.isStartDocument()) {
+                    employee = new Employee();
+                    continue;
+                }
+                if (event.isStartElement()) {
+                    StartElement startElement = event.asStartElement();
+                    Attribute attribute;
 
-    }
+                    switch (startElement.getName().getLocalPart()) {
+                        case "Person":
+                            attribute = startElement.getAttributeByName(new QName("Type"));
+                            if (attribute != null) {
+                                employee.setType(EmployeeType.valueOf(attribute.getValue().toUpperCase()));
+                            }
+                            break;
+                        case "FirstName":
+                            event = reader.nextEvent();
+                            employee.setFirstName(event.asCharacters().getData());
+                            break;
+                        case "SecondName":
+                            event = reader.nextEvent();
+                            employee.setSecondName(event.asCharacters().getData());
+                            break;
+                        case "Department":
+                            event = reader.nextEvent();
+                            employee.setDepartment(event.asCharacters().getData());
+                            break;
+                        case "EmploymentDate":
+                            event = reader.nextEvent();
+                            try {
+                                LocalDate date = LocalDate.parse(event.asCharacters().getData());
+                                employee.setEmploymentDate(date);
+                            } catch (Exception e) {
+                            }
+                            break;
+                        case "ResignationDate":
+                            event = reader.nextEvent();
+                            try {
+                                LocalDate date = LocalDate.parse(event.asCharacters().getData());
+                                employee.setResignationDate(date);
+                            } catch (Exception e) {
+                            }
+                            break;
+                        case "Group":
+                            group = new Group();
+                            employee.getGroups().add(group);
 
-    private static Employee parseEmployee(XMLEventReader reader) throws XMLStreamException {
-        Employee employee = null;
-        Group group = null;
-        Contact contact = null;
+                            attribute = startElement.getAttributeByName(new QName("Location"));
+                            if (attribute != null) {
+                                group.setLocation(Location.valueOf(attribute.getValue().toUpperCase()));
+                            }
 
-        while (reader.hasNext()) {
-            XMLEvent event = reader.nextEvent();
-            if (event.isStartDocument()) {
-                employee = new Employee();
-                continue;
-            }
-            if (event.isStartElement()) {
-                StartElement startElement = event.asStartElement();
-                Attribute attribute;
+                            attribute = startElement.getAttributeByName(new QName("Name"));
+                            if (attribute != null) {
+                                group.setName(GroupName.valueOf(attribute.getValue().toUpperCase()));
+                            }
+                            break;
+                        case "Role":
+                            event = reader.nextEvent();
+                            group.setRole(event.asCharacters().getData());
+                            break;
+                        case "From":
+                            event = reader.nextEvent();
+                            try {
+                                LocalDate date = LocalDate.parse(event.asCharacters().getData());
+                                group.setFrom(date);
+                            } catch (Exception e) {
+                            }
+                            break;
+                        case "To":
+                            event = reader.nextEvent();
+                            try {
+                                LocalDate date = LocalDate.parse(event.asCharacters().getData());
+                                group.setTo(date);
+                            } catch (Exception e) {
+                            }
+                            break;
+                        case "Contact":
+                            contact = new Contact();
+                            employee.getContacts().add(contact);
 
-                switch (startElement.getName().getLocalPart()) {
-                    case "Person":
-                        attribute = startElement.getAttributeByName(new QName("Type"));
-                        if (attribute != null) {
-                            employee.setType(EmployeeType.valueOf(attribute.getValue().toUpperCase()));
-                        }
-                        break;
-                    case "FirstName":
-                        event = reader.nextEvent();
-                        employee.setFirstName(event.asCharacters().getData());
-                        break;
-                    case "SecondName":
-                        event = reader.nextEvent();
-                        employee.setSecondName(event.asCharacters().getData());
-                        break;
-                    case "Department":
-                        event = reader.nextEvent();
-                        employee.setDepartment(event.asCharacters().getData());
-                        break;
-                    case "EmploymentDate":
-                        event = reader.nextEvent();
-                        try {
-                            LocalDate date = LocalDate.parse(event.asCharacters().getData());
-                            employee.setEmploymentDate(date);
-                        } catch (Exception e) {
-                        }
-                        break;
-                    case "ResignationDate":
-                        event = reader.nextEvent();
-                        try {
-                            LocalDate date = LocalDate.parse(event.asCharacters().getData());
-                            employee.setResignationDate(date);
-                        } catch (Exception e) {
-                        }
-                        break;
-                    case "Group":
-                        group = new Group();
-                        employee.getGroups().add(group);
+                            attribute = startElement.getAttributeByName(new QName("Type"));
+                            if (attribute != null) {
+                                ContactType contactType = ContactType.valueOf(attribute.getValue().toUpperCase());
+                                contact.setType(contactType);
 
-                        attribute = startElement.getAttributeByName(new QName("Location"));
-                        if (attribute != null) {
-                            group.setLocation(Location.valueOf(attribute.getValue().toUpperCase()));
-                        }
-
-                        attribute = startElement.getAttributeByName(new QName("Name"));
-                        if (attribute != null) {
-                            group.setName(GroupName.valueOf(attribute.getValue().toUpperCase()));
-                        }
-                        break;
-                    case "Role":
-                        event = reader.nextEvent();
-                        group.setRole(event.asCharacters().getData());
-                        break;
-                    case "From":
-                        event = reader.nextEvent();
-                        try {
-                            LocalDate date = LocalDate.parse(event.asCharacters().getData());
-                            group.setFrom(date);
-                        } catch (Exception e) {
-                        }
-                        break;
-                    case "To":
-                        event = reader.nextEvent();
-                        try {
-                            LocalDate date = LocalDate.parse(event.asCharacters().getData());
-                            group.setTo(date);
-                        } catch (Exception e) {
-                        }
-                        break;
-                    case "Contact":
-                        contact = new Contact();
-                        employee.getContacts().add(contact);
-
-                        attribute = startElement.getAttributeByName(new QName("Type"));
-                        if (attribute != null) {
-                            contact.setType(ContactType.valueOf(attribute.getValue().toUpperCase()));
-                        }
-                        break;
-                    case "Value":
-                        event = reader.nextEvent();
-                        contact.setValue(event.asCharacters().getData());
-                        break;
+                                // fill Map<ContactType, List<Contact>>
+                                List<Contact> contactsByType = employee.getContactsByType()
+                                        .getOrDefault(contactType, new ArrayList<>());
+                                contactsByType.add(contact);
+                                employee.getContactsByType().put(contactType, contactsByType);
+                            }
+                            break;
+                        case "Value":
+                            event = reader.nextEvent();
+                            contact.setValue(event.asCharacters().getData());
+                            break;
+                    }
                 }
             }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
         return employee;
---
+++ src/test/java/dev/gl/xml/stax/EmployeeStAXParserTests.java
@@ -0,0 +1,101 @@
+package dev.gl.xml.stax;
+
+import dev.gl.xml.employee.Contact;
+import dev.gl.xml.employee.ContactType;
+import dev.gl.xml.employee.Employee;
+import dev.gl.xml.employee.Group;
+import dev.gl.xml.employee.GroupName;
+import dev.gl.xml.employee.Location;
+import dev.gl.xml.utils.ExamplesStorage;
+import java.io.File;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+
+/**
+ *
+ * @author gl
+ */
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class EmployeeStAXParserTests {
+
+    private static EmployeeStAXParser parser;
+    private static Employee employee;
+
+    @BeforeAll
+    static void init() {
+        File file = ExamplesStorage.getFile(ExamplesStorage.EMPLOYEE_XML_PATH);
+        parser = new EmployeeStAXParser(file);
+    }
+
+    @Test
+    @Order(1)
+    void employeeExists() {
+        employee = parser.parseEmployee();
+        assumeTrue(employee != null, "Employee should exist");
+    }
+    
+    @Test
+    void testFirstName() {
+        assertEquals("Oscar", employee.getFirstName(), "FirstName should be \"Oscar\"");
+    }
+
+    @Test
+    void testEmploymentDate() {
+        assertEquals(LocalDate.of(2024, Month.JUNE, 1), employee.getEmploymentDate(),
+                "EmploymentDate should be 2024.06.01");
+    }
+
+    @Test
+    void testResignationDate() {
+        assertNull(employee.getResignationDate(), "Resignation date should be null");
+    }
+
+    @Test
+    void testGroupsNumber() {
+        assertEquals(2, employee.getGroups().size(), "There should be two groups");
+    }
+
+    @Test
+    void testGroupContent() {
+        Group group = employee.getGroups().get(0);
+        assertAll("Check all content of the first group",
+                () -> assertEquals(Location.LIVERPOOL, group.getLocation()),
+                () -> assertEquals(GroupName.BUDGETING_COMMON, group.getName()),
+                () -> assertEquals("Participant", group.getRole()),
+                () -> assertEquals(LocalDate.of(2024, Month.JUNE, 1), group.getFrom()),
+                () -> assertEquals(LocalDate.of(2025, Month.FEBRUARY, 28), group.getTo()));
+    }
+
+    @Test
+    void testContactsNumber() {
+        assertAll("Check contacts completeness",
+                () -> assertEquals(3, employee.getContacts().size(), "There should be three contacts"),
+                () -> assertEquals(1, employee.getContactsByType().get(ContactType.PHONE).size()),
+                () -> assertEquals(1, employee.getContactsByType().get(ContactType.EMAIL).size()),
+                () -> assertEquals(1, employee.getContactsByType().get(ContactType.FACEBOOK).size()));
+    }
+
+    @Test
+    void testContactsContect() {
+        Map<ContactType, List<Contact>> contactsByType = employee.getContactsByType();
+
+        Contact phone = contactsByType.get(ContactType.PHONE).get(0);
+        Contact email = contactsByType.get(ContactType.EMAIL).get(0);
+        Contact facebook = contactsByType.get(ContactType.FACEBOOK).get(0);
+
+        assertAll("Check all three contact types",
+                () -> assertEquals("123456789", phone.getValue()),
+                () -> assertEquals("o_adams@my_test_company.qazqaz", email.getValue()),
+                () -> assertEquals("https://www.facebook.com/oscar_adams_liverpool_my_test_company/", facebook.getValue()));
+    }
+}
